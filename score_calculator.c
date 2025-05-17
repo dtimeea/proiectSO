b2ed084d63e56ecdd44e8923a30d5d40df9384fb
dtimeea <dobreantimeea@gmail.com>
2025-05-17 21:20:56 +0300

    nou commit

---
+++ score_calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "biblioteca.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2){
+        fprintf(stderr, "Utilizare: %s <hunt_id>\n", argv[0]);
+        exit(1);
+    }
+
+    char *hunt_id = argv[1];  //primim id-ul vanatorii(din linia de comanda)
+    char path[256];
+    snprintf(path, sizeof(path), "%s/date", hunt_id); 
+
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        perror("eroare la deschiderea fisierului de comori");
+        exit(1);
+    }
+
+    Treasure t;
+    int total_score =0;
+
+    while (read(fd, &t, sizeof(Treasure)) == sizeof(Treasure)) {
+        total_score += t.value; //adunam valoarea fiecarei comori
+    }
+
+    close(fd);
+
+    printf("%s : scor total = %d\n", hunt_id, total_score);
+    return 0;
+}
